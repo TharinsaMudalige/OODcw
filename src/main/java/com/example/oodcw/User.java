13d228e8ea9a31ce9d3b9290cc78ed1be4716f31
TharinsaMudalige <venuki.20232784@iit.ac.lk>
2024-11-12 23:15:30 +0530

    user class

---
+++ src/main/java/com/example/oodcw/User.java
@@ -0,0 +1,13 @@
+package com.example.oodcw;
+
+
+public class User {
+    private String userID;
+    private String userPassword;
+
+    public User(String userID, String userPassword) {
+        this.userID = userID;
+        this.userPassword = userPassword;
+    }
+
+}
