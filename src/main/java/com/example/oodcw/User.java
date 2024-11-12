13e2f080b9001e4eb45725ea8f63e1d1003d02cb
TharinsaMudalige <venuki.20232784@iit.ac.lk>
2024-11-12 23:44:41 +0530

    sign up controller

--- src/main/java/com/example/oodcw/User.java
+++ src/main/java/com/example/oodcw/User.java
@@ -1,13 +1,47 @@
 package com.example.oodcw;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
-    private String userID;
+    private String userID;  //the username of the user
     private String userPassword;
+    private List<String> likedArticles;  //stores a list of liked articles
 
     public User(String userID, String userPassword) {
         this.userID = userID;
         this.userPassword = userPassword;
+        this.likedArticles = new ArrayList<>();
     }
+
+    //Getters and Setters
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUserPassword(){
+        return userPassword;
+    }
+
+    public List<String> getLikedArticles() {
+        return likedArticles; //return a list of all the liked articles
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public void addLikedArticles(String article) {
+        likedArticles.add(article);  //Adding a liked article to the list
+    }
+
+
+
+
 
 }
