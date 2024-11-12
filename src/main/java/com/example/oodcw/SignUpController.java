b489228689c4afa0ff4425fa3beda3e0a0030bde
TharinsaMudalige <venuki.20232784@iit.ac.lk>
2024-11-12 19:24:43 +0530

    sign up Controller

---
+++ src/main/java/com/example/oodcw/SignUpController.java
@@ -0,0 +1,11 @@
+package com.example.oodcw;
+
+import javafx.event.ActionEvent;
+
+public class SignUpController {
+    public void OnCreateAccountButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void OnLoginButtonClick(ActionEvent actionEvent) {
+    }
+}
