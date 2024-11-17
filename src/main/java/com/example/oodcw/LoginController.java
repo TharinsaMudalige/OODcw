2397c96e1705c0c6c9a41e9c96a24094fa23d730
TharinsaMudalige <venuki.20232784@iit.ac.lk>
2024-11-18 02:06:06 +0530

    login controller

--- src/main/java/com/example/oodcw/LoginController.java
+++ src/main/java/com/example/oodcw/LoginController.java
@@ -6,12 +6,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class LoginController {
+public class LoginController extends BaseController {
     @FXML
     private TextField logUsernameText;
     @FXML
@@ -21,8 +22,33 @@ public class LoginController {
     @FXML
     private PasswordField logPasswordText;
 
+    private DatabaseHandler databaseHandler;
 
-    public void OnLoginButton3Click(ActionEvent actionEvent) throws Exception {
+    public LoginController() {
+        databaseHandler = new DatabaseHandler();
+    }
+
+
+    public void OnLoginButton3Click(ActionEvent actionEvent){
+        String username = logUsernameText.getText();
+        String password = logPasswordText.getText();
+
+        if(username.isEmpty() || password.isEmpty()){
+            showAlertMessage(Alert.AlertType.ERROR, "Error!", "Username or Password is empty");
+            return;
+        }
+
+        if(!databaseHandler.isUsernameExists(username)){
+            showAlertMessage(Alert.AlertType.ERROR, "Error!", "Username is invalid");
+            return;
+        }
+
+        if(!databaseHandler.isPasswordCorrect(username, password)){
+            showAlertMessage(Alert.AlertType.ERROR, "Error!", "Password is incorrect");
+            return;
+        }
+
+        navigateToArticleDisplay(actionEvent,username);
     }
 
     public void OnCreateAccountButtonClick2(ActionEvent actionEvent) throws Exception {
@@ -37,16 +63,28 @@ public class LoginController {
         stage.show();
     }
 
+    public void navigateToArticleDisplay(ActionEvent actionEvent, String username){
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("article-view.fxml"));
+            Parent ArticleViewWindow = loader.load();
+
+            ArticleViewController articleViewController = loader.getController();
+            articleViewController.setUsername(username);
+
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setTitle("Explore Articles");
+            Scene scene = new Scene(ArticleViewWindow,948,720);
+            stage.setScene(scene);
+
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public void OnBackToMainMenuButtonClick(ActionEvent actionEvent) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("smartRead-view.fxml"));
-        Parent BackToMenu = loader.load();
-
-        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        stage.setTitle("Personalised News Recommendation System");
-        Scene scene = new Scene(BackToMenu,940,720);
-        stage.setScene(scene);
-
-        stage.show();
+        GoToMainMenu(actionEvent);
     }
 
     public void OnResetPasswordButtonClick(ActionEvent actionEvent) throws Exception{
