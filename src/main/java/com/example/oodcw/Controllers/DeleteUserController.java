aeec4f280621b29b3b592e0227696c7abe2523fc
TharinsaMudalige <venuki.20232784@iit.ac.lk>
2024-12-03 01:15:39 +0530

    admin delete user controller

---
+++ src/main/java/com/example/oodcw/Controllers/DeleteUserController.java
@@ -0,0 +1,13 @@
+package com.example.oodcw.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class DeleteUserController {
+    @FXML
+    private TextField userID;
+
+    public void OnBackButtonClick(ActionEvent actionEvent) {
+    }
+}
